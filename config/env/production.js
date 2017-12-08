5f3ae0783a4a0b63e55d1b88254a8bbbd7b0abd1
Sarawut <sarawut_yeen@hotmail.com>
2017-12-08 18:54:27 +0700

    add node for login

---
+++ config/env/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+    //  mongoUri: 'mongodb://localhost/myFirstNode',
+      // or use 
+       mongoUri: 'mongodb://admin:password@ds133746.mlab.com:33746/marsshop',
+      debug: true,
+      sessionSecret: 'dev_secret_key'
+  }
