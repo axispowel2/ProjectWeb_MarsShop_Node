5b6b9880e3a6ff9a721d07e1bd3d3cd2362ea807
Sarawut <sarawut_yeen@hotmail.com>
2017-12-10 12:09:01 +0700

    add facebook login

--- config/env/development.js
+++ config/env/development.js
@@ -1,19 +1,24 @@
 module.exports = {
 
-   //mongoUri: 'mongodb://localhost:27017/project',
-    // or use 
-     mongoUri: 'mongodb://admin:password@ds133746.mlab.com:33746/marsshop',
-    debug: true,
-    sessionSecret: 'dev_secret_key',
-    
-    google: {
-      clientID: '98896871793-bdekdb22i6gpsnai9moccugnpv88i21v.apps.googleusercontent.com',
-      clientSecret: 'V9TKaf-6J9ieiwHsRsxLB24R',
-      callbackURL: 'http://localhost:3000/oauth/google/callback'
-},
-twitter: {
-  consumerKey: 'xMmRqQb9CC5yfCsEf6IM643lr',
-  consumerSecret: 'CgFLR4A0UDFZFTCJeglMvanaHvePXBqiVX6kh7cklNsemeWnov',
-  callbackURL: 'http://marsshop.herokuapp.com/oauth/twitter/callback'
-}
+  //mongoUri: 'mongodb://localhost:27017/project',
+  // or use 
+  mongoUri: 'mongodb://admin:password@ds133746.mlab.com:33746/marsshop',
+  debug: true,
+  sessionSecret: 'dev_secret_key',
+
+  google: {
+    clientID: '98896871793-bdekdb22i6gpsnai9moccugnpv88i21v.apps.googleusercontent.com',
+    clientSecret: 'V9TKaf-6J9ieiwHsRsxLB24R',
+    callbackURL: 'http://marsshop.herokuapp.com/oauth/google/callback'
+  },
+  facebook: {
+    clientID: '308687199634879',
+    clientSecret: '4d9d680f6fc59dc2cf7047fdf0d429da',
+    callbackURL: 'http://marsshop.herokuapp.com/oauth/facebook/callback'
+  },
+  twitter: {
+    consumerKey: 'xMmRqQb9CC5yfCsEf6IM643lr',
+    consumerSecret: 'CgFLR4A0UDFZFTCJeglMvanaHvePXBqiVX6kh7cklNsemeWnov',
+    callbackURL: 'http://marsshop.herokuapp.com/oauth/twitter/callback'
+  }
 }
---
+++ config/strategies/facebook.js
@@ -0,0 +1,27 @@
+import passport from 'passport';
+var FacebookStrategy = require('passport-facebook-oauth').OAuth2Strategy;
+import config from '../config.js';
+import user from '../../app/controllers/user.controller';
+
+module.exports = () => {
+    passport.use(new FacebookStrategy({
+        clientID: config.facebook.clientID,
+        clientSecret: config.facebook.clientSecret,
+        callbackURL: config.facebook.callbackURL,
+        passReqToCallback: true
+    }, function (req, accessToken, refreshToken, profile, done) {
+        var providerData = profile._json;
+        providerData.accessToken = accessToken;
+        providerData.refreshToken = refreshToken;
+        var providerUserProfile = {
+            firstName: profile.name.givenName,
+            lastName: profile.name.familyName,
+            email: profile.emails[0].value,
+            username: profile.displayName,
+            provider: 'facebook',
+            providerId: profile.id,
+            providerData: providerData
+        }
+        user.saveOAuthUserProfile(req, providerUserProfile, done);
+    }));
+}
