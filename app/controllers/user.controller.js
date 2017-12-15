1a2d08206724566c53afc4d71892e6b7e1c36b49
Kritnatee Panintrarux <kong.panin@gmail.com>
2017-12-15 18:30:44 +0700

    test delete

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -33,7 +33,7 @@ exports.getUsers = (req, res, next) => {
 exports.getSelectUser = (req, res) => {
     res.json(req.user);
 }
-
+*/
 exports.deleteUser = (req, res, next) => {
     User.findOneAndRemove({ username: req.user.username }, req.body,
         (err, user) => {
@@ -42,58 +42,58 @@ exports.deleteUser = (req, res, next) => {
             } else {
                 res.json(user);
             }
-        });*/
-//------------------------------------------------
-exports.create = (req, res, next) => {
-    var user = new User(req.body);
-    user.save((err) => {
-        if (err) {
-            console.log('Failure');
-            return next(err);
-        }
-        else {
-            console.log('Success');
-            res.json(user);
-        }
-    });
-}
-
-exports.login = (req, res) => {
-    if (!req.user) {
-        res.sendFile((path.join(__dirname + '/../views/login.html')));
-    }
-    else {
-        return res.redirect('/home');
-    }
-}
-
-exports.logout = (req, res) => {
-    req.logout();
-    res.redirect('/');
-}
-
-
-exports.saveOAuthUserProfile = (req, profile, done) => {
-    User.findOne({
-        provider: profile.provider,
-        providerId: profile.providerId
-    }, function (err, user) {
-        if (err) return done(err);
-        else {
-            if (!user) {
-                var possibleUsername = profile.username
-                    || (profile.email ? profile.email.split('@')[0] : '');
-                console.log('NAME: ' + profile.username);
-                User.findUniqueUsername(possibleUsername, null, (availableUsername) => {
-                    profile.username = availableUsername;
-                    user = new User(profile);
-                    user.save((err) => {
-                        if (err) { return req.res.redirect('/login'); }
-                        return done(err, user);
-                    })
-                });
+        });
+    //------------------------------------------------
+    exports.create = (req, res, next) => {
+        var user = new User(req.body);
+        user.save((err) => {
+            if (err) {
+                console.log('Failure');
+                return next(err);
             }
-            else { return done(err, user); }
+            else {
+                console.log('Success');
+                res.json(user);
+            }
+        });
+    }
+
+    exports.login = (req, res) => {
+        if (!req.user) {
+            res.sendFile((path.join(__dirname + '/../views/login.html')));
         }
-    });
-}
+        else {
+            return res.redirect('/home');
+        }
+    }
+
+    exports.logout = (req, res) => {
+        req.logout();
+        res.redirect('/');
+    }
+
+
+    exports.saveOAuthUserProfile = (req, profile, done) => {
+        User.findOne({
+            provider: profile.provider,
+            providerId: profile.providerId
+        }, function (err, user) {
+            if (err) return done(err);
+            else {
+                if (!user) {
+                    var possibleUsername = profile.username
+                        || (profile.email ? profile.email.split('@')[0] : '');
+                    console.log('NAME: ' + profile.username);
+                    User.findUniqueUsername(possibleUsername, null, (availableUsername) => {
+                        profile.username = availableUsername;
+                        user = new User(profile);
+                        user.save((err) => {
+                            if (err) { return req.res.redirect('/login'); }
+                            return done(err, user);
+                        })
+                    });
+                }
+                else { return done(err, user); }
+            }
+        });
+    }
